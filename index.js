7e590f1a49da213f711cc9428b4774299e9deab1
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-26 14:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay (arrMusicians, arrInstruments) {
   var listOfMusicianAndPlay = [];
   for (var i = 0; i < arrMusicians.length; i++){
-    listOfMusician.push(arrMusicians[i] + " plays " arrInstruments[i]);
+    listOfMusician.push(arrMusicians[i] + " plays " + arrInstruments[i]);
     //listOfMusician.push(`${arrMusicians[i]} plays ${arrInstruments[i]}`);
   }
   return(listOfMusicianAndPlay);
