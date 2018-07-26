e64c2a55579448658fddeeb0e9c9ba4b0efb8aaf
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-26 12:04:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // add solution here
 function theBeatlesPlay (arrMusicians, arrInstruments) {
   var newArray = new Array;
+  for (user in arrMusicians) {
+    for (inst in arrInstruments){
+      newArray.push(`${user} plays ${inst}`);
+    }
+  }
 }
 
 function johnLennonFacts () {
